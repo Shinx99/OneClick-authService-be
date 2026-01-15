ab1e2f5d7f97157e915c13bf257a1502e537aae1
Shinx99 <nguyennam.zarakhan@gmail.com>
2026-01-15 17:08:24 +0700

    initial commit

---
+++ src/main/java/com/oneClick/authService_be/AuthServiceBeApplication.java
@@ -0,0 +1,13 @@
+package com.oneClick.authService_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneClick/authService_be/AuthServiceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneClick.authService_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
