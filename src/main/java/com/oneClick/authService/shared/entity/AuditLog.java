1484b8e1cc86e5aab5e7a1b903ccc77b2417c861
Shinx99 <nguyennam.zarakhan@gmail.com>
2026-02-25 18:06:57 +0700

    fix(auditLog): fix variable for auditLog -#16

--- src/main/java/com/oneClick/authService/shared/entity/AuditLog.java
+++ src/main/java/com/oneClick/authService/shared/entity/AuditLog.java
@@ -1,7 +1,7 @@
 // shared/entity/AuditLog.java
 package com.oneClick.authService.shared.entity;
 
-import com.oneClick.authService.shared.entity.BaseEntity;
+import com.oneClick.authService.shared.entity.BaseAuditEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -12,25 +12,29 @@ import java.time.Instant;
 
 @Entity
 @Table(name = "auth_audit_logs")
+@AttributeOverrides({
+        @AttributeOverride(name = "id", column = @Column(name = "audit_id")),
+
+})
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class AuditLog extends BaseEntity {
+public class AuditLog extends BaseAuditEntity {
 
     @Column(name = "account_id", nullable = true)
-    Long accountId;
+    private Long accountId;
 
     @Column(name = "event_type", nullable = false, length = 50)
-    String eventType;  // LOGIN_SUCCESS, REGISTER, PASSWORD_RESET...
+    private String eventType;  // LOGIN_SUCCESS, REGISTER, PASSWORD_RESET...
 
     @Column(name = "ip", columnDefinition = "inet")
-    String ip;
+    private String ip;
 
     @Column(name = "user_agent", columnDefinition = "text")
-    String userAgent;
+    private String userAgent;
 
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(name = "meta", columnDefinition = "jsonb", nullable = true)
